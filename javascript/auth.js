b4309a706b80d75c6a835ad9124056e424c9fe9f
CHARANKUMAR-N <innovatewithcharan2024@gmail.com>
2025-05-30 15:46:32 +0530

    Update auth.js

--- javascript/auth.js
+++ javascript/auth.js
@@ -5,7 +5,7 @@ const auth0Config = {
     domain: "dev-mlvc4obj0xoj262o.us.auth0.com",
     clientId: "msFAoItlh3wmSPTOfpTDkhFcwVuniIND",
     audience: "https://api.employeemanagement.com",
-    redirectUri: window.location.origin + "/employee-management-frontend/index.html"
+    redirectUri: window.location.origin + "/index.html"
 };
 
 // Initialize Auth0 client
